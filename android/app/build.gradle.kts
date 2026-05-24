814f6134465f5d3708266085f53f96f2baa61ca8
weijia <weijia@users.noreply.github.com>
2026-05-24 02:44:12 +0000

    fix: 使用 tasks.withType 设置 Kotlin JVM target
    kotlinOptions 在 Kotlin DSL android 块中不可用
    改用 tasks.withType<KotlinCompile> 方式

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,15 +14,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.obsidiangit.obsidian_git"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,10 +24,14 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
